01a58cf046a4e3ce56327e2b8f29cff7115e5e1e
NaoYamanouchi <nao.yamanouchi@ga.aais-lab.org>
2024-06-13 19:15:14 +0900

    Change: separation function

--- BriefFunction/DirFile.py
+++ BriefFunction/DirFile.py
@@ -1,30 +1,6 @@
 import os
 import shutil
 
-# Write
-def Write_a_1dlist(filepath,writelist,delimiter):
-    writelist = [str(i) for i in writelist]
-    with open(filepath,'a') as f:
-        f.write(delimiter.join(writelist))
-
-def Write_a_2dlist(filepath,writelist,delimiter):
-    for row in writelist:
-        row = [str(i) for i in row]
-        with open(filepath,'a') as f:
-            f.write(delimiter.join(row)+"\n")
-
-def Write_w_1dlist(filepath,writelist,delimiter):
-    writelist = [str(i) for i in writelist]
-    with open(filepath,'w') as f:
-        f.write(delimiter.join(writelist))
-
-def Write_w_2dlist(filepath,writelist,delimiter):
-    Clear_file(filepath)
-    for row in writelist:
-        row = [str(i) for i in row]
-        with open(filepath,'a') as f:
-            f.write(delimiter.join(row)+"\n")
-
 # Clear
 def Clear_dir(path):
     shutil.rmtree(path)
--- BriefFunction/other.py
+++ BriefFunction/other.py
@@ -2,7 +2,6 @@ import itertools
 import re
 import time
 
-
 def Conv_2dListTo1dList(l_2d):
     return list(itertools.chain.from_iterable(l_2d))
 
