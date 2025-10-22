bb4eccd13c934903274b0b916f162c7a882dc66c
NaoYamanouchi <nao.yamanouchi@ga.aais-lab.org>
2025-10-22 16:52:32 +0900

    add: csv read var type in getData & calc.py

--- src/happyset/getData.py
+++ src/happyset/getData.py
@@ -1,17 +1,50 @@
 import csv
 from pathlib import Path
+from .other import Conv_2dListTo1dList
 
 # csv
 def Get_csv2List(path :str|Path) -> list:
     with open(path, 'r') as csvfile:
         reader = csv.reader(csvfile)
-        data = [data for data in reader]
+        rows = [d for d in reader]
+    data = []        
+    for row in rows:
+        tmp = []
+        for d in row:
+            if _is_type_int(d):
+                tmp.append(int(d))
+            elif _is_type_float(d):
+                tmp.append(float(d))
+            else:
+                tmp.append(d)
+        data.append(tmp)
     return data
 
 # text
 def Get_text2list(path :str|Path, delimiter :str) -> list:
     with open(path, 'r') as f:
         return f.read().split(delimiter)
+
+def _is_type_int(target: str) -> bool:
+    if not type(target) in [str, int]:
+        return False
+    try:
+        int(target)
+        return True
+    except ValueError:
+        return False
+
+def _is_type_float(target: str) -> bool:
+    if not type(target) in [str, float]:
+        return False
+    if _is_type_int(target):
+           return False
+       
+    try:
+        float(target)
+        return True
+    except ValueError:
+        return False
 
 # directory
 def Get_dirList(path :str|Path) -> list:
